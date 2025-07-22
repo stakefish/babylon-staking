ece8e375f6138a2d195d2dfd834c81b3291a98a7
Michal Baranowski <baranovskimichal@gmail.com>
2025-07-22 16:44:01 +0700

    chore: amend readme btn styles

--- src/ui/common/components/SfBanner/SfBanner.tsx
+++ src/ui/common/components/SfBanner/SfBanner.tsx
@@ -1,5 +1,7 @@
 import { PiWarningOctagonFill } from "react-icons/pi";
 
+import { Button } from "@/ui/components/Button";
+
 export const SfBanner = () => {
   return (
     <div className="flex flex-row gap-2 p-2 m-4 bg-backgroundWarningDefault border border-itemWarningMute text-sm text-itemWarningDefault items-center justify-between">
@@ -9,14 +11,16 @@ export const SfBanner = () => {
           <strong>Important Update for Early Stakers:</strong> For users staked
           before 10 April 2025, you must register to join Phase 2 to earn
           rewards.{" "}
-          <a
+          <Button
+            variant="text"
+            size="sm"
+            endIcon={{ iconKey: "share", size: 12 }}
+            className="normal-case font-normal underline hover:no-underline"
             href="https://blog.stake.fish/how-to-stake-bitcoin-with-stakefish-on-babylon-phase-2"
-            target="_blank"
             rel="noopener noreferrer"
-            className="underline hover:no-underline"
           >
             Read more
-          </a>
+          </Button>
         </div>
       </div>
     </div>
