d43033be43432d4ec283fe954452497a31c3484b
Michal Baranowski <baranovskimichal@gmail.com>
2025-07-25 15:08:14 +0700

    fix: banner ion on mobile

--- src/ui/common/components/SfBanner/SfBanner.tsx
+++ src/ui/common/components/SfBanner/SfBanner.tsx
@@ -6,7 +6,9 @@ export const SfBanner = () => {
   return (
     <div className="flex flex-row gap-2 p-2 m-4 bg-backgroundWarningDefault border border-itemWarningMute text-sm text-itemWarningDefault items-center justify-between">
       <div className="flex flex-row gap-2 items-center">
-        <PiWarningOctagonFill size={18} />
+        <div>
+          <PiWarningOctagonFill size={18} />
+        </div>
         <div className="font-sans">
           <strong>Important Update for Early Stakers:</strong> For users staked
           before 10 April 2025, you must register to join Phase 2 to earn
